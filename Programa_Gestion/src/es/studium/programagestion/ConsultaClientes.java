27916f16e33762cff5ec82b26f21159f8bf3bad0
mjesusoj <m.jesusojedasalvador@gmail.com>
2019-05-01 13:08:03 +0200

    Solucionada la tabla ya con sus campos y todo, falta solo imprimir la tabla en un PDF

--- Programa_Gestion/src/es/studium/programagestion/ConsultaClientes.java
+++ Programa_Gestion/src/es/studium/programagestion/ConsultaClientes.java
@@ -1,43 +1,38 @@
 package es.studium.programagestion;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.Image;
-import java.awt.Label;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class ConsultaClientes extends Frame implements ActionListener, WindowListener{
 
 	private static final long serialVersionUID = 1L;
 
 	Label lblConsulta = new Label("Consulta Clientes");
+
+	Button btnImprimir = new Button("Imprimir");
 	
 	JTable tablaClientes = new JTable();
 
-	Button btnVolver = new Button("Volver");
+	String [] titulos= {"DNI","Nombre del Cliente","Apellidos del Cliente"};
 
-	String [] titulos= {"DNI","Nombre","Apellidos"};
-
+	DefaultTableModel modelo = new DefaultTableModel();
+	
 	// Base de Datos
 	static String driver = "com.mysql.jdbc.Driver";
 	static String url = "jdbc:mysql://localhost:3306/farmaciapr2?autoReconnect=true&useSSL=false";
 	static String login = "admin";
 	static String password = "Studium2018;";
-	static String sentencia = "SELECT * FROM clientes;";
+	static String sentencia = "SELECT dniCliente, nombreCliente ,apellidosCliente FROM clientes ORDER BY 1;";
 	static Connection connection = null;
 	static Statement statement = null;
 	static ResultSet rs = null;
@@ -50,12 +45,13 @@ public class ConsultaClientes extends Frame implements ActionListener, WindowLis
 		setLayout(new FlowLayout());
 		add(lblConsulta);
 		tablaClientes = new JTable(rellenarTabla(),titulos);
+		tablaClientes.setModel(modelo);
 		add(new JScrollPane(tablaClientes), BorderLayout.CENTER);
-		add(btnVolver);
+		add(btnImprimir);
 		setSize(500,600);
 		setLocationRelativeTo(null);
 		addWindowListener(this);
-		btnVolver.addActionListener(this);
+		btnImprimir.addActionListener(this);
 		setResizable(false);
 		setVisible(true);
 	}
@@ -65,7 +61,7 @@ public class ConsultaClientes extends Frame implements ActionListener, WindowLis
 		Image miIcono = mipantalla.getImage("src//farmacia.png");
 		setIconImage(miIcono);
 	}
-
+	
 	public Object[][] rellenarTabla() {
 		try 
 		{
@@ -74,16 +70,24 @@ public class ConsultaClientes extends Frame implements ActionListener, WindowLis
 			statement = connection.createStatement();						
 			rs = statement.executeQuery(sentencia);
 			
+			ResultSetMetaData rsMd = rs.getMetaData();
+			// Guardar en una variable las columnas que hay
+			int cantidadColumnas = rsMd.getColumnCount();
+			
+			// Bucle para ir de 1 hasta las columnas que existen
+			for (int i=1;i<=cantidadColumnas;i++) {
+				// Añadir los títulos de las columnas
+				modelo.addColumn(rsMd.getColumnLabel(i));
+			}
+			
 			while (rs.next()) {
-				String dniCliente = rs.getString("dniCliente");
-				String nombreCliente = rs.getString("nombreCliente");
-				String apellidosCliente = rs.getString("apellidosCliente");
-				
-				Object [][] datosFilaFinal= {
-						{dniCliente, nombreCliente, apellidosCliente}
-				};
-				
-				return datosFilaFinal;
+				Object [] fila = new Object[cantidadColumnas];
+				for (int i=0;i<cantidadColumnas;i++) {
+					// Coger los objetos de la bd	
+					fila[i] = rs.getObject(i+1);
+				}
+				// Añadir las columnas
+				modelo.addRow(fila);
 			}
 		}
 
@@ -113,13 +117,30 @@ public class ConsultaClientes extends Frame implements ActionListener, WindowLis
 		}
 		return null;
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if(btnVolver.equals(arg0.getSource())){
-			this.setVisible(false);
-			new MenuPrincipal(null);
+		if(btnImprimir.equals(arg0.getSource())){
+			try {
+			String prueba =  btnImprimir.getLabel();
+			System.out.println(prueba);
+			//Se obtiene el objeto PrintJob
+			PrintJob pjob = this.getToolkit().getPrintJob(this, prueba, null);
+			//Se obtiene el objeto graphics sobre el que pintar
+			Graphics pg = pjob.getGraphics();
+			//Se fija la fuente de caracteres con la que escribir
+			pg.setFont(new Font("Arial",Font.PLAIN,12));
+			//Se escribe el mensaje del Cuadro de Texto indicando
+			//posición con respecto a la parte superior izquierda
+			pg.drawString(prueba,100,100);
+			//Se finaliza la página
+			pg.dispose();
+			//Se hace que la impresora termine el trabajo y suelte la página
+			pjob.end();
+		}catch(NullPointerException npe) {
+			npe.printStackTrace();
 		}
+	}
 	}
 
 	@Override
@@ -132,19 +153,14 @@ public class ConsultaClientes extends Frame implements ActionListener, WindowLis
 
 	@Override
 	public void windowActivated(WindowEvent arg0) {}
-
 	@Override
 	public void windowClosed(WindowEvent arg0) {}
-
 	@Override
 	public void windowDeactivated(WindowEvent arg0) {}
-
 	@Override
 	public void windowDeiconified(WindowEvent arg0) {}
-
 	@Override
 	public void windowIconified(WindowEvent arg0) {}
-
 	@Override
 	public void windowOpened(WindowEvent arg0) {}
 }
